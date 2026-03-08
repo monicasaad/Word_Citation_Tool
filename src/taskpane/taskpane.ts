a65663c2cdb1e032f06807bee2a6c42945552416
Monica Saad <monicasaad@hotmail.ca>
2026-03-08 05:55:58 -0400

    feat: implemented get selected text operation

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -12,3 +12,12 @@ export async function insertText(text: string) {
     console.log("Error: " + error);
   }
 }
+
+export async function getSelectedText() {
+  return Word.run(async (context) => {
+    const selection = context.document.getSelection();
+    selection.load("text");
+    await context.sync();
+    return selection.text.trim();
+  });
+}
